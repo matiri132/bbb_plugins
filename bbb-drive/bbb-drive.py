aa3610ca20f9e83b40a1e6b8b0eaec5dab2db59f
mxchxdx <matiasmachado7@gmail.com>
2020-11-13 03:19:04 -0300

    bbb-drive working

--- bbb-drive/bbb-drive.py
+++ bbb-drive/bbb-drive.py
@@ -1,51 +1,86 @@
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-import pprint
 import httplib2
 import googleapiclient.http
 import oauth2client.client
+import pprint
+import sys
+import os
+import drivefunc as drivef
 
-
-# Path to the Service account cred file
-SERVICE_ACCOUNT_FILE = 'service.json'
-
-# Path to the file to upload.
-FILENAME = 'document.txt'
-# Metadata about the file.
-MIMETYPE = 'text/plain'
-TITLE = 'doc1'
-DESCRIPTION = 'A shiny new text document about hello world.'
+SERVERLIST = "serverlist.xml"
 
 def main():
+ 
+  # Path to the Service account cred file
+  SERVICE_ACCOUNT_FILE = 'service.json'
+  if not os.path.exists(SERVICE_ACCOUNT_FILE):
+    print("No credentials file found. Get credentials and save in the directory as service.json")    
+    return -1
 
-    #OAUTH 2 Credentials
-    print("Creating credentials...")
-    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE)
-    scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/drive'])
+  #Verify args
+  if(len(sys.argv) < 3):
+    print('ERROR: No file especified. Syntaxis: bbb-drive filename folderName')
+    return -1
 
-    #service creation
-    print("Creating service...")
-    drive_service = build('drive', 'v3', credentials=scoped_credentials)
-    
-    print("Settingup the request...")
-    # Insert a file. Files are comprised of contents and metadata.
-    # MediaFileUpload abstracts uploading file contents from a file on disk.
-    media_body = googleapiclient.http.MediaFileUpload(
-        FILENAME,
-        mimetype=MIMETYPE,
-        resumable=True
-    )
-    # The body contains the metadata for the file.
-    body = {
-      'name': TITLE,
-      'description': DESCRIPTION,
-    }
-    print("Uploading file...")
-    # Perform the request and print the result.
-    new_file = drive_service.files().create(body=body, media_body=media_body).execute()
-    pprint.pprint(new_file)
-    
-    drive_service.close()
+# Path to the file to upload passed as argument.
+  FILENAME = str(sys.argv[1])
+  if not os.path.exists(FILENAME):
+    print('ERROR: Especified file does not exist.')    
+    return -1
+
+  # Metadata about the file
+  MIMETYPE = drivef.get_mimeType(FILENAME)
+  if(MIMETYPE == -1):
+    print('Unkwown file format. Use \'mp4\' for videos or \'txt\' for text files.')
+    return -1
+
+  #Create drive service based on service account credentials
+  drive_service = drivef.initialize_drive(SERVICE_ACCOUNT_FILE)
+
+  #get metadata from bbb recording
+  metadata = drivef.get_metaData(str(sys.argv[2]))
+
+  #Get bbb-recordings folder id from Drive
+  FOLDER_SV_ID = drivef.get_folderId(drive_service, 
+              drivef.get_folderName(SERVERLIST, metadata['server-name']))
+  if(FOLDER_SV_ID == -1):
+    print("Folder associated to server isn't exist, verify serverlist.xml")
+    return -1
+  #Verify sub folder existence
+  if not (drivef.verify_folder( drive_service, FOLDER_SV_ID , metadata['context'])):
+    print("Failed to create folder on drive...")
+    return -1
+  PARENT_ID = drivef.get_folderId(drive_service , metadata['context'])
+
+  #Verify file existence
+  TITLE = str(metadata['name']) + ":" + str(metadata['id'])
+  if( drivef.verify_file(drive_service, TITLE , MIMETYPE, PARENT_ID)):
+    print("File already uploaded...")
+    return 1
+
+  DESCRIPTION = metadata['description']
+
+  print("Settingup the request...")
+  # Insert a file. Files are comprised of contents and metadata.
+  # MediaFileUpload abstracts uploading file contents from a file on disk.
+  media_body = googleapiclient.http.MediaFileUpload(
+      FILENAME,
+      mimetype=MIMETYPE,
+      resumable=True
+  )
+  # The body contains the metadata for the file.
+  body = {
+    'name': TITLE,
+    'description': DESCRIPTION,
+    'parents' : [PARENT_ID]
+  }
+  print("Uploading file...")
+  # Perform the request and print the result.
+  new_file = drive_service.files().create(body=body, media_body=media_body).execute()
+  pprint.pprint(new_file)
+  
+  drive_service.close()
 
 if __name__ == '__main__':
   main()
---
+++ bbb-drive/drivefunc.py
@@ -0,0 +1,193 @@
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+import httplib2
+import googleapiclient.http
+import oauth2client.client
+from apiclient import errors
+import os
+import io
+import xml.etree.ElementTree as ET
+
+def initialize_drive(sv_acc_cred):
+  """[summary] Initialize a google drive service from a google service
+      account credential.
+  Args:
+      sv_acc_cred ([credential]): [OAuth2 credential 
+      for service account]
+
+  Returns:
+      [service]: [Google drive API service]
+  """
+  #OAUTH 2 Credentials
+  credentials = service_account.Credentials.from_service_account_file(sv_acc_cred)
+  scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/drive'])
+  #service creation
+  drive = build('drive', 'v3', credentials=scoped_credentials)
+  return drive
+
+def delete_file(service, file_id):
+  """[summary] Delete a file with file_id from a 
+                google drive service object.
+
+  Args:
+      service ([google api service]): [google drive service object]
+      file_id ([type]): [id from the file to delete]
+  """
+  try:
+    service.files().delete(fileId=file_id).execute()
+  except (errors.HttpError) as error:
+    print('An error occurred: %s' % error)
+
+def download_file(files , output):
+  """[sumary] Download a file from a drive
+
+  Args:
+      files ([google drive file object]): [file object to download]
+  """
+  fh = io.FileIO(output ,'wb')
+  downloader = googleapiclient.http.MediaIoBaseDownload(fh, files)
+  done = False
+  while done is False:
+      status, done = downloader.next_chunk()
+      print("Download %d%%." % int(status.progress() * 100))
+
+def get_mimeType(filename):
+  """[summary] Get mimetype from a file, accept txt and mp4 files.
+
+  Args:
+      filename ([string]): [Filename of the file to get the mimetype]
+
+  Returns:
+      [string]: [mimetype from the selected file]
+  """
+  root_ext = os.path.splitext(filename)
+  if(root_ext[1] == '.mp4'):
+    return 'video/mp4'
+  elif(root_ext[1] == '.txt'):
+    return 'text/plain'
+  else:
+    return -1
+
+def get_folderName(filename, servername):
+  """[Get folder name from servername]
+
+  Args:
+      filename ([str]): [File with all servers and folders]
+      servername ([str]): [Server name]
+
+  Returns:
+      [str]: [Folder name]
+  """
+  tree = ET.parse(filename)
+  root = tree.getroot()
+  for elem in root:
+    for subelem in elem:
+      if(subelem.attrib['name'] == servername):
+        return str(subelem.text)
+
+
+def get_folderId(service, folderName):
+  """[Return folderId by folder name]
+
+  Args:
+      service ([google_service]): [drive service]
+      folderName ([string]): [Folder name]
+
+  Returns:
+      [string]: [folder id]
+  """
+  while True:
+    response = service.files().list(q="mimeType='application/vnd.google-apps.folder'",
+                                          spaces='drive',
+                                          fields='nextPageToken, files(id, name)',
+                                          pageToken=None).execute()
+    for file in response.get('files', []):
+        # Process change
+        if(file.get('name') == folderName):
+          return file.get('id')
+    page_token = response.get('nextPageToken', None)
+    if page_token is None:
+        return -1
+
+def get_metaData(filename):
+  """[Get metadata from XML file] 
+
+  Args:
+      filename ([str]): [Metada filename]
+
+  Returns:
+      [dictionary]: [Dictionary with usefull fields]
+  """
+  tree = ET.parse(filename)
+  root = tree.getroot()
+  metadata = {}
+  # total amount of items
+  for elem in root:
+    if(elem.tag == 'id'):
+      metadata['id'] = str(elem.text)
+    for subelem in elem:
+      if(subelem.tag == 'bbb-context'):
+        #Class name
+        metadata['context'] = str(subelem.text)
+      elif(subelem.tag == 'bbb-recording-name'):
+        metadata['name'] = str(subelem.text)
+      elif(subelem.tag == 'bbb-recording-description'):
+        metadata['description'] = str(subelem.text)
+      elif(subelem.tag == 'bbb-origin-server-name'):
+        metadata['server-name'] = str(subelem.text)      
+  return metadata
+
+def verify_folder(service , parentId, foldername):
+  """[Verify existence of a folder, if not exist its created]
+
+  Args:
+      service ([google_service]): [Google drive service object]
+      parentId ([str]): [Parent folder id]
+      foldername ([str]): [Folder name to search]
+
+  Returns:
+      [type]: [description]
+  """
+  page_token = None
+  while True:
+    response = service.files().list(q="mimeType='application/vnd.google-apps.folder'",
+                                          spaces='drive',
+                                          fields='nextPageToken, files(name, id, parents)',
+                                          pageToken=page_token).execute()
+    for file in response.get('files', []):
+      # Process change
+      parents = file.get('parents')
+      if((file.get('name') == foldername) and (parents[0] == parentId)):
+        return True
+    page_token = response.get('nextPageToken', None)
+    if page_token is None:
+        break
+  file_metadata = {
+    'name': str(foldername),
+    'mimeType': 'application/vnd.google-apps.folder',
+    'parents' : [str(parentId)]
+  }
+  file = service.files().create(body=file_metadata,
+                                fields='id').execute()
+  if( file != None):
+    return True
+  else:
+    return False
+
+def verify_file(service, filename, mimeType, parentId):
+  page_token = None
+  mimeTy = "mimeType=\'" + str(mimeType) + "\'"
+  while True:
+    response = service.files().list(q=mimeTy,
+                                    spaces='drive',
+                                    fields='nextPageToken, files(name, parents)',
+                                    pageToken=page_token).execute()
+    for file in response.get('files', []):
+      # Process change
+      parents = file.get('parents')
+      if((file.get('name') == filename) and (parents[0] == parentId)):
+        return True
+    page_token = response.get('nextPageToken', None)
+    if page_token is None:
+        break
+  return False
