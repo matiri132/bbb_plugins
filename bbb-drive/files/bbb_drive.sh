75f9ddef9563b802cf387fbac9d0adbc4e78c66d
mxchxdx <matiasmachado7@gmail.com>
2020-12-15 15:28:43 -0300

    log fix

--- bbb-drive/files/bbb_drive.sh
+++ bbb-drive/files/bbb_drive.sh
@@ -44,7 +44,7 @@ do
     FILENAME_PATH=""
     UPLOAD=""
     
-    for videofile in $(ls -tr "${PATH_CONV}/*.mp4")
+    for videofile in $(ls -tr ${PATH_CONV}/*.mp4)
         do
             FILENAME=$(/usr/bin/basename "${videofile}" | /usr/bin/cut -f 1 -d '.')
             EXT=$(/usr/bin/basename "${videofile}" | /usr/bin/cut -f 2 -d '.') 
