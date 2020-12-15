71f0ab92c4bb06e3de2d6df301d8a3ac506c947e
mxchxdx <matiasmachado7@gmail.com>
2020-12-15 15:44:47 -0300

    log fix

--- bbb-drive/files/bbb-drive.py
+++ bbb-drive/files/bbb-drive.py
@@ -8,64 +8,78 @@ import sys
 import os
 import drivefunc as drivef
 import datetime
+import logging
+
+log_format = (
+    '[%(asctime)s] %(levelname)-8s %(name)-12s %(message)s')
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=log_format,
+    filename=('/var/log/bbb_drive_debug.log'),
+)
+logger = logging.getLogger("bbb-drive.py")
 
 SERVERLIST = "serverlist.xml"
 
 def main():
- 
+  
+  
      #Verify args
   if(len(sys.argv) < 3):
-    print('ERROR: [Errno: 01] No file especified. Syntaxis: bbb-drive filename metadatafile')
+    logger.error('[Errno: 01] No file especified. Syntaxis: bbb-drive filename metadatafile')
     return -1
 
+  logger.info('Init bbb-drive info: ' + str(sys.argv[1]) + " - " + str(sys.argv[2]) )
   # Path to the Service account cred file
   SERVICE_ACCOUNT_FILE = 'service.json'
   if not os.path.exists(SERVICE_ACCOUNT_FILE):
-    print("ERROR: [Errno: 00] No credentials file found. Get credentials and save in the directory as service.json")    
+    logger.error("[Errno: 00] No credentials file found. Get credentials and save in the directory as service.json")    
     return -1
 
  # Path to the file to upload passed as argument.
   FILENAME = str(sys.argv[1])
   if not os.path.exists(FILENAME):
-    print("ERROR: [Errno: 02] Especified file does not exist. ->" + str(FILENAME))    
+    logger.error("[Errno: 02] Especified file does not exist. ->" + str(FILENAME))    
     return -1
 
   if not os.path.exists(sys.argv[2]):
-    print("ERROR: [Errno: 02] Especified file does not exist. ->" + str(sys.argv[2]))    
+    logger.error("[Errno: 02] Especified file does not exist. ->" + str(sys.argv[2]))    
     return -1
 
+  
   # Metadata about the file
   MIMETYPE = drivef.get_mimeType(FILENAME)
   if(MIMETYPE == -1):
-    print("ERROR: [Errno: 03] Unkwown file format. ->" + str(FILENAME) + "  ->" + str(MIMETYPE)  )
+    logger.error("[Errno: 03] Unkwown file format. ->" + str(FILENAME) + "  ->" + str(MIMETYPE)  )
     return -1
 
   #get metadata from bbb recording
   metadata = drivef.get_metaData(str(sys.argv[2]))
-
+  
   #Create drive service based on service account credentials
   drive_service = drivef.initialize_drive(SERVICE_ACCOUNT_FILE)
-  
+  logger.info('Verifying drive folders')
   #Get bbb-recordings folder id f rom Drive
   FOLDER_NAME = drivef.get_folderName(SERVERLIST, metadata['server-name'])
   FOLDER_SV_ID = drivef.get_folderId(drive_service,FOLDER_NAME )
   if(FOLDER_SV_ID == -1 or FOLDER_NAME == -1):
-    print("ERROR: [Errno: 04] FolderERROR ->" +"FN:" +str(FOLDER_NAME) + "FID:" + str(FOLDER_SV_ID) + "-" + metadata['server-name'] 
+    logger.error("[Errno: 04] FolderERROR ->" +"FN:" +str(FOLDER_NAME) + "FID:" + str(FOLDER_SV_ID) + "-" + metadata['server-name'] 
             + "  " + str(FILENAME))
     return -1
   #Verify sub folder existence
   if not (drivef.verify_folder( drive_service, FOLDER_SV_ID , metadata['context'])):
-    print("ERROR: [Errno: 05] Failed to create folder on drive. "  + str(FILENAME))
+    logger.error("[Errno: 05] Failed to create folder on drive. "  + str(FILENAME))
     return -1
   PARENT_ID = drivef.get_folderId(drive_service , metadata['context'])
 
-  
+  logger.info('Verifying file to upload')
   #Verify file existence
   s = int(metadata['start-time'])/1000-21600
   date = datetime.datetime.fromtimestamp(s).strftime('%d-%m-%Y_%H:%M')
   NAME = date + "-" + str(metadata['id']) + ".mp4"
   if( drivef.verify_file(drive_service, NAME)):
-    print("ERROR: [Errno: 06] File already uploaded -> " + str(FILENAME))
+    logger.error("[Errno: 06] File already uploaded -> " + str(FILENAME))
     return 1
 
   # Insert a file. Files are comprised of contents and metadata.
@@ -81,6 +95,7 @@ def main():
     'parents' : [PARENT_ID]
   }
   # Perform the request and print the result.
+  logger.info('Uploading...')
   new_file = drive_service.files().create(body=body, media_body=media_body).execute()
   pprint.pprint(new_file)
   
